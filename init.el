20dd6474495c7075dddbcce21ce081224d8b8a69
Alex Broekhof <abroekhof@nestlabs.com>
2017-07-05 17:26:08 -0700

    Update for Google

--- init.el
+++ init.el
@@ -5,21 +5,20 @@
 ;;; Code:
 ;;; initalize all ELPA packages
 (require 'package)
-
-; list the repositories containing them
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-
-; activate all the packages (in particular autoloads)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
-; install use-package
-(if (not (package-installed-p 'use-package))
-    (progn
-      (package-refresh-contents)
-      (package-install 'use-package)))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(require 'use-package)
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
 
 (use-package org
   :ensure t)
@@ -39,7 +38,6 @@
  '(custom-safe-themes
    (quote
     ("61724add6ddc09a0426f6b39109a76dd83174cf36d76563d3a35c7e07c0753c6" "e7ec0cc3ce134cc0bd420b98573bbd339a908ac24162b8034c98e1ba5ee1f9f6" default)))
- '(magit-use-overlays nil)
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
