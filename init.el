7a61495a11c133c317fd3c440e457bed018a9666
Alex Broekhof <abroekhof@nestlabs.com>
2017-07-13 13:32:22 -0700

    Change theme and add smart-mode-line

--- init.el
+++ init.el
@@ -37,11 +37,15 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("61724add6ddc09a0426f6b39109a76dd83174cf36d76563d3a35c7e07c0753c6" "e7ec0cc3ce134cc0bd420b98573bbd339a908ac24162b8034c98e1ba5ee1f9f6" default)))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "61724add6ddc09a0426f6b39109a76dd83174cf36d76563d3a35c7e07c0753c6" "e7ec0cc3ce134cc0bd420b98573bbd339a908ac24162b8034c98e1ba5ee1f9f6" default)))
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (adaptive-wrap zenburn-theme web-mode volatile-highlights virtualenvwrapper use-package undo-tree switch-window smartparens rainbow-delimiters paradox multiple-cursors magit js2-mode helm-themes helm-swoop helm-projectile helm-descbinds helm-ag guide-key flycheck exec-path-from-shell diff-hl csv-mode company-anaconda buffer-move anzu)))
- '(paradox-github-token t))
+    (git-gutter neotree doom-themes adaptive-wrap zenburn-theme web-mode volatile-highlights virtualenvwrapper use-package undo-tree switch-window smartparens rainbow-delimiters paradox multiple-cursors magit js2-mode helm-themes helm-swoop helm-projectile helm-descbinds helm-ag guide-key flycheck exec-path-from-shell diff-hl csv-mode company-anaconda buffer-move anzu)))
+ '(paradox-github-token t)
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
