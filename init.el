257823ee419b9d7b8229aa7998134c11491bdc91
Alex Broekhof <abroekhof@google.com>
2019-08-13 18:19:41 -0700

    Clean things up a bit

--- init.el
+++ init.el
@@ -4,26 +4,24 @@
 
 ;;; Code:
 ;;; initalize all ELPA packages
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
 (eval-when-compile
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package)
+    (package-install 'diminish)
+    (package-install 'bind-key))
+
+  (setq use-package-always-ensure t)
+  (setq use-package-expand-minimally t)
+
   (require 'use-package))
-
-(use-package diminish
-  :ensure t)
 (require 'diminish)
-
-(use-package bind-key
-  :ensure t)
 (require 'bind-key)
 
 (use-package org
